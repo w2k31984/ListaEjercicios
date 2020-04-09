f67e71e1925b510487ea66114f2ff401d93b2e9d
Cristian Parada <cmpmendoza12@gmail.com>
2020-04-09 15:14:34 -0600

    Creacion de Componente Ejercicios

---
+++ src/app/ejercicios/ejercicios.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicios',
+  templateUrl: './ejercicios.component.html',
+  styleUrls: ['./ejercicios.component.css']
+})
+export class EjerciciosComponent implements OnInit {
+
+  ejercicio = 'Flexion'
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
